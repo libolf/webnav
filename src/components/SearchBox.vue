c153e4a11d595ddf15fee8165b911aacb2c7b4a9
libo <1021211327@qq.com>
2026-05-08 10:17:14 +0800

    修改先关闭搜索建议再打开搜索页面

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -151,10 +151,12 @@ const selectSuggest = (text) => {
 
 const handleInputFocus = () => {
   if (suggestions.value.length > 0) {
+    console.log("handle input focus")
     showSuggest.value = true
   } else {
     const searchHistory = store.getSearchHistory()
     if (searchHistory.length > 0) {
+      console.log("handle input focus history")
       showSuggest.value = true
       suggestions.value = searchHistory
     }
@@ -169,6 +171,7 @@ watch(keyword, (newVal) => {
   if (!newVal.trim() && !skipShowSearchHistory) {
     const searchHistory = store.getSearchHistory()
     if (searchHistory.length > 0) {
+      console.log("watch history")
       showSuggest.value = true
       suggestions.value = searchHistory
     } else {
@@ -188,6 +191,7 @@ watch(keyword, (newVal) => {
         // 确保异步返回时，用户输入的内容没有被清空，再更新列表
         if (document.hasFocus() && keyword.value.trim()) {
           suggestions.value = data
+          console.log("suggest")
           showSuggest.value = suggestions.value.length > 0
         }
       }
